78d8f2f50d14d34d72d6f4abce1448e0520fc604
cibuild <pawan.gandhi.ci@gmail.com>
2013-06-07 16:03:46 -0400

    Revert "Rest Spring - first commit"
    This reverts commit 2dffbadf7a287872666aa1f1627b5cc36df5c62f.

--- src/main/java/com/welflex/service/NoSeatAvailableException.java
+++
@@ -1,9 +0,0 @@
-package com.welflex.service;
-
-public class NoSeatAvailableException extends Exception {
-  private static final long serialVersionUID = 1L;
-
-  public NoSeatAvailableException(String message) {
-    super(message);
-  }
-}
--- src/main/java/com/welflex/service/NoSuchAirportException.java
+++
@@ -1,9 +0,0 @@
-package com.welflex.service;
-
-public class NoSuchAirportException extends Exception {
-  private static final long serialVersionUID = 1L;
-
-  public NoSuchAirportException(String message) {
-    super(message);
-  }
-}
